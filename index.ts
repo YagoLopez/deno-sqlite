cb19150c79bfd6921ebffc8545716900bd2f96f8
Yago Lopez <yago.lopez@gmail.com>
2022-01-12 18:23:18 +0100

    Refactor index.ts

--- index.ts
+++ index.ts
@@ -1,26 +1,25 @@
-import { DB } from 'https://deno.land/x/sqlite/mod.ts'
+// @ts-ignore
+import { connect } from 'https://deno.land/x/sqlite_shell/mod.ts'
 
-// Open a database
-const db = new DB('test.db')
-db.query(`
-  CREATE TABLE IF NOT EXISTS people (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT
-  )
-`)
+// Connect to a database
+const db = await connect('test.db')
+await db.execute(
+  'CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)',
+)
 
 const names = ['Peter Parker', 'Clark Kent', 'Bruce Wayne']
 
 // Run a simple query
-// for (const name of names as any) {
-//   db.query("INSERT INTO people (name) VALUES (?)", [name]);
-// }
+// @ts-ignore
+for (const name of names) {
+  await db.execute('INSERT INTO people (name) VALUES (?)', [name])
+}
 
 // Print out data in table
 // @ts-ignore
-for (const [name] of db.query('SELECT name FROM people')) {
+for await (const {name} of db.query('SELECT name FROM people')) {
   console.log(name)
 }
 
 // Close connection
-db.close()
+await db.close()
